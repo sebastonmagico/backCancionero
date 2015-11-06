d40be959c9a9556a58f74b159fde71b9dd89bdde
Sebastian Tobon <sebastonmagico@yahoo.es>
2015-11-06 17:26:12 +0100

    Un poco mejor organizado.

---
+++ constants.js
@@ -0,0 +1,8 @@
+// constants.js
+// ========
+module.exports = {
+    port: 9500,
+    token: "gDhmWP1FmwXWIeR5j6Xv5We3JnAwY8unOIumLlAC:X",
+    bibleURL: "es.bibles.org",
+    biblePath: "/v2/verses/spa-RVR1960"
+};
--- server.js
+++ server.js
@@ -1,54 +1,59 @@
-//Lets require/import the HTTP module
+/**
+ * Main server
+ */
 var http = require('http');
 var https = require('https');
-var psamls = require('./psalms.js');
-
 var base64 = require('base-64');
 var Buffer = require('buffer').Buffer;
+var psamls = require('./psalms.js');
+var constants = require('./constants.js');
 
-const PORT=9500;
-const TOKEN="gDhmWP1FmwXWIeR5j6Xv5We3JnAwY8unOIumLlAC:X";
-
-var auth = new Buffer(TOKEN).toString('base64');
-
+var auth = new Buffer(constants.token).toString('base64');
 
 var options = {
-    host: 'es.bibles.org',
+    host: constants.bibleURL,
     port: 443,
-    path: '/v2/verses/spa-RVR1960:' + psamls.randomChapter(),
-    //path: '/v2/versions.js',
+    path: constants.biblePath + psamls.randomChapter(),
     headers: {
         'Authorization': 'Basic ' + auth
     }
 };
 
 var str = '';
+
 var final = {
     text:"",
     reference:""
 };
 
 function resetData(){
-  options.path = '/v2/verses/spa-RVR1960:' + psamls.randomChapter();
+  options.path = constants.biblePath + psamls.randomChapter();
   str = '';
 };
+
 
 //We need a function which handles requests and send response
 function handleRequest(request, response){
 
+    //Config headers
     response.setHeader('Access-Control-Allow-Origin', '*');
     response.setHeader('Access-Control-Request-Method', '*');
     response.setHeader('Access-Control-Allow-Methods', 'GET');
 
+    //Access controlled by requested url
     switch (request.url){
         case "/get_verse":
+            //Reset my response
             resetData();
             response.writeHeader(200);
+
+            //Making the curl request and waiting
             https.request(options, function(r){
                 r.on('data', function(chunk){
                     str += chunk;
                 });
                 r.on('end', function(){
+                    //Resolving with json parsed string
                     str = JSON.parse(str);
                     final.text = str.response.verses[0].text;
                     final.reference = str.response.verses[0].reference;
@@ -59,6 +64,7 @@ function handleRequest(request, response){
             }).end();
             break;
         default:
+            //Not allowed buddy.
             response.writeHeader(404, {"Content-Type": "text/plain"});
             response.write("Not found");
             response.end();
@@ -71,6 +77,5 @@ var server = http.createServer(handleRequest);
 
 //Lets start our server
 server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
     console.log("Server listening on: http://localhost:%s", PORT);
 });      
