1409dc903a8dc66061910fa7970d8ab31d0a7cc0
Sebastian Tobon <s.tobon@barceloviajes.com>
2015-12-14 17:05:02 +0100

    Listo para versión golden 0.1

--- constants.js
+++ constants.js
@@ -4,5 +4,5 @@ module.exports = {
     port: 9500,
     token: "gDhmWP1FmwXWIeR5j6Xv5We3JnAwY8unOIumLlAC:X",
     bibleURL: "es.bibles.org",
-    biblePath: "/v2/verses/spa-RVR1960"
+    biblePath: "/v2/verses/spa-RVR1960:"
 };
